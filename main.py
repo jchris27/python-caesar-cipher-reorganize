71a5e4d7065a724601ea253ded67e0dc07446ae2
Jin <53550425+jchris27@users.noreply.github.com>
2022-03-30 08:13:56 +0000

    edit caesar function

--- main.py
+++ main.py
@@ -6,25 +6,40 @@ shift = int(input("Type the shift number:\n"))
 
 #TODO-1: Combine the encrypt() and decrypt() functions into a single function called caesar(). 
 
-def encrypt(plain_text, shift_amount):
-  cipher_text = ""
-  for letter in plain_text:
-    position = alphabet.index(letter)
-    new_position = position + shift_amount
-    cipher_text += alphabet[new_position]
-  print(f"The encoded text is {cipher_text}")
-
-def decrypt(cipher_text, shift_amount):
+def caesar(direction, text, shift):
+  if direction == "decode":
+    shift *= -1
   plain_text = ""
-  for letter in cipher_text:
-    position = alphabet.index(letter)
-    new_position = position - shift_amount
-    plain_text += alphabet[new_position]
-  print(f"The decoded text is {plain_text}")
 
-if direction == "encode":
-  encrypt(plain_text=text, shift_amount=shift)
-elif direction == "decode":
-  decrypt(cipher_text=text, shift_amount=shift)
+  for letter in text:
+    position = alphabet.index(letter)
+    new_position = position + shift
+    plain_text += alphabet[new_position]
+  print(f"The {direction}d text is {plain_text}")
+
+
+    
+# def encrypt(plain_text, shift_amount):
+#   cipher_text = ""
+#   for letter in plain_text:
+#     position = alphabet.index(letter)
+#     new_position = position + shift_amount
+#     cipher_text += alphabet[new_position]
+#   print(f"The encoded text is {cipher_text}")
+
+# def decrypt(cipher_text, shift_amount):
+#   plain_text = ""
+#   for letter in cipher_text:
+#     position = alphabet.index(letter)
+#     new_position = position - shift_amount
+#     plain_text += alphabet[new_position]
+#   print(f"The decoded text is {plain_text}")
+
+# if direction == "encode":
+#   encrypt(plain_text=text, shift_amount=shift)
+# elif direction == "decode":
+#   decrypt(cipher_text=text, shift_amount=shift)
 
 #TODO-2: Call the caesar() function, passing over the 'text', 'shift' and 'direction' values.
+
+caesar(direction=direction,text=text,shift=shift)
